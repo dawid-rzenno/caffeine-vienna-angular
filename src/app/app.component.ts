f465a5fe6abf8de439c172b5e0779d880642e3b5
Dawid Rzenno <dawid.rzenno@protonmail.com>
2024-08-21 18:56:26 +0200

    Rename the project to Vienna

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AUTH_DIRECT_ROUTE, AuthDirectRouteKey } from "./features/common/constants/auth-direct-route.const";
 
 @Component({
-  selector: 'app-root',
+  selector: 'vienna-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
